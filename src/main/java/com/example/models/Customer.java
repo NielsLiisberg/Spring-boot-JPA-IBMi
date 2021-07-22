eb8a188d8eff6b611e328772d2e7e2e05252289d
Niels Liisberg <nli@system-method.com>
2021-07-22 18:34:08 +0200

    Auto generated JPA class

--- src/main/java/com/example/models/Customer.java
+++ src/main/java/com/example/models/Customer.java
@@ -1,52 +1,69 @@
+// This code is build by Sitemule JPA generator:
+// https://gist.github.com/NielsLiisberg/c3b356fcfc969e6d1125fd3d38806036
 package com.example.models;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-// Be careful with @Data. Equals in lombok is not compatible with spring.
-@Data
+import lombok.Getter;
+import lombok.Setter;
+
+// Be careful with @Data. Equals/hash in lombok is not compatible with spring.
+@Getter
+@Setter
 @Entity
 @Table(name="QCUSTCDT")
 public class Customer {
+
+	// Datatype: NUMERIC, name: cusnum, fieldLabel: Customer number field, len: 6, prec: 0
 	@Id
 	@Column(name="CUSNUM")
-	long id;
+	Long id ;
 
+	// Datatype: CHAR, name: lstnam, fieldLabel: Last name field, len: 8
 	@Column(name="LSTNAM")
-	String lastName;
+	String lastName ;
 
+	// Datatype: CHAR, name: init, fieldLabel: First and middle initial field, len: 3
 	@Column(name="INIT")
-	String initials;
+	String initials ;
 
+	// Datatype: CHAR, name: street, fieldLabel: Street address field, len: 13
 	@Column(name="STREET")
-	String  streetName;
+	String streetName ;
 
+	// Datatype: CHAR, name: city, fieldLabel: City field, len: 6
 	@Column(name="CITY")
-	String cityName;
+	String cityName ;
 
+	// Datatype: CHAR, name: state, fieldLabel: State abbreviation field, len: 2
 	@Column(name="STATE")
-	String stateName;
+	String stateName ;
 
+	// Datatype: NUMERIC, name: zipcod, fieldLabel: Zip code field, len: 5, prec: 0
 	@Column(name="ZIPCOD")
-	String zipCode;
+	Long zipCode ;
 
+	// Datatype: NUMERIC, name: cdtlmt, fieldLabel: Credit limit field, len: 4, prec: 0
 	@Column(name="CDTLMT")
-	BigDecimal creditLimit;
+	Long creditLimit ;
 
+	// Datatype: NUMERIC, name: chgcod, fieldLabel: Charge code field, len: 1, prec: 0
 	@Column(name="CHGCOD")
-	Long chargeCode;
+	Integer chargeCode ;
 
+	// Datatype: NUMERIC, name: baldue, fieldLabel: Balance due field, len: 6, prec: 2
 	@Column(name="BALDUE")
-	BigDecimal balanceDue;
+	BigDecimal balanceDue ;
 
+	// Datatype: NUMERIC, name: cdtdue, fieldLabel: Credit due field, len: 6, prec: 2
 	@Column(name="CDTDUE")
-	BigDecimal creditDue;
+	BigDecimal creditDue ;
 
 }
-
-
-
