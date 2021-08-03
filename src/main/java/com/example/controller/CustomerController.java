7d73beec922168011615dd544868f165cafdea87
Niels Liisberg <nli@system-method.com>
2021-08-03 14:43:59 +0200

    added use cases

--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -16,21 +16,25 @@ public class CustomerController {
 	@Autowired
 	private CustomerRepository customerRepository;
 
+	// use case: http://localhost:8888/api/hello
 	@GetMapping("/hello")
 	String demo1() {
 		return "Hello world";
 	}
 
+	// use case: http://localhost:8888/api/customer/839283
 	@GetMapping("/customer/{id}")
 	Optional<Customer> getCustomer(@PathVariable("id") Long id) {
 		return customerRepository.findById(id);
 	}
 
+	// use case: http://localhost:8888/api/customerList
 	@GetMapping("/customerList")
 	Iterable<Customer> getCustomerListAll() {
 		return  customerRepository.findAll();
 	}
 
+	// use case: http://localhost:8888/api/customerSearch?search=john
 	@GetMapping("/customerSearch")
 	List<Customer> getCustomerListSearch(@RequestParam("search") String search) {
 		return  customerRepository.findBySearch(search );
